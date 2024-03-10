510b30cf63c260ece9765e45982af9c2e5ed2d27
Sean Burman <sean.burman@icloud.com>
2024-03-10 16:57:56 +0000

    Updated vars

--- config/env.go
+++ config/env.go
@@ -1,11 +1,7 @@
 package config
 
 import (
-	"flag"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 // Type Vars
@@ -22,12 +18,12 @@ type Vars struct {
 // Env() returns Vars struct of environment variables
 func Env() Vars {
 	// Load if not a test. This isn't required during testing.
-	if flag.Lookup("test.v") == nil {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading environment variables")
-		}
-	}
+	// if flag.Lookup("test.v") == nil {
+	// 	err := godotenv.Load()
+	// 	if err != nil {
+	// 		log.Fatal("Error loading environment variables")
+	// 	}
+	// }
 
 	return Vars{
 		PORT: os.Getenv("PORT"),
